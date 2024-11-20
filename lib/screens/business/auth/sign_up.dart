20ad73d387e847c5cb0d62b5602a11e076fc7f5b
Hassan Talha <hassan.talha@techchaps.com>
2024-11-20 12:20:41 +0500

    fix: resolved Sigup/sigin ui issues and functionality

--- lib/screens/business/auth/sign_up.dart
+++ lib/screens/business/auth/sign_up.dart
@@ -1,5 +1,6 @@
 //imports
 import 'package:flutter/material.dart';
+import 'package:thrive_hub/core/utils/email_validator.dart';
 import '../../../core/constants/text_styles.dart';
 import 'sign_in.dart';
 import '../../../widgets/input_fields.dart';
@@ -48,9 +49,24 @@ class _BusinessSignUpScreenState extends State<BusinessSignUpScreen> {
     _confirmPasswordController.dispose();
     super.dispose();
   }
-
+  void _validateEmail() {
+    final email = _emailController.text.trim();
+    final validationError = emailValidator(email);
+    if (validationError != null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(validationError),
+          backgroundColor: Colors.black,
+        ),
+      );
+      return;
+    }
+    Navigator.pushNamed(context, '/business-profile-setup');
+  }
   @override
   Widget build(BuildContext context) {
+    final passwordsMatch =
+        _passwordController.text == _confirmPasswordController.text;
     return Scaffold(
       backgroundColor: Color(0xFFF4F4F4),
       body: SingleChildScrollView(
@@ -70,9 +86,26 @@ class _BusinessSignUpScreenState extends State<BusinessSignUpScreen> {
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        Container(
+          width: MediaQuery.of(context).size.width, // Full screen width
+          height: MediaQuery.of(context).size.height, // Full screen height
+          decoration: BoxDecoration(
+            color: Colors.white, // Background color of the form
+            // borderRadius: BorderRadius.only(
+            //   topLeft: Radius.circular(20), // Rounded top-left corner
+            //   topRight: Radius.circular(20), // Rounded top-right corner
+            // ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3), // Shadow position
+              ),
+            ],
+          ),
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -88,43 +121,63 @@ class _BusinessSignUpScreenState extends State<BusinessSignUpScreen> {
                   SizedBox(height: 12),
                   CustomInputField(
                     labelText: 'Password',
+                    hintText:
+                    'Create Password', // Pass custom hint text here
                     controller: _passwordController,
                     obscureText: _obscureText1,
                     suffixIcon: IconButton(
-                      icon: Icon(_obscureText1 ? Icons.visibility_off : Icons.visibility),
+                      icon: Icon(
+                        _obscureText1
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        color: _obscureText1
+                            ? Color(0xFFACB5BB)
+                            : Colors.black,
+                      ),
                       onPressed: () {
                         setState(() {
                           _obscureText1 = !_obscureText1;
                         });
                       },
                     ),
+                    borderColor: passwordsMatch
+                        ? Color(0xFFEDF1F3)
+                        : Color(0xFF0C9809), // Dynamic border color
                   ),
+
                   SizedBox(height: 12),
                   CustomInputField(
                     labelText: 'Confirm Password',
+                    hintText:
+                    'Repeat your password', // Pass custom hint text here
                     controller: _confirmPasswordController,
                     obscureText: _obscureText2,
                     suffixIcon: IconButton(
-                      icon: Icon(_obscureText2 ? Icons.visibility_off : Icons.visibility),
+                      icon: Icon(
+                        _obscureText2
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        color: _obscureText2
+                            ? Color(0xFFACB5BB)
+                            : Colors.black,
+                      ),
                       onPressed: () {
                         setState(() {
                           _obscureText2 = !_obscureText2;
                         });
                       },
                     ),
+                    borderColor: passwordsMatch
+                        ? Color(0xFFEDF1F3)
+                        : Color(0xFF0C9809), // Dynamic border color
                   ),
                   SizedBox(height: 24),
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
                       onPressed: _isButtonEnabled
-                          ? () {
+                          ? _validateEmail:null,
                         // Add your onPressed code here!
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Account creation logic goes here.')),
-                        );
-                      }
-                          : null,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Color(0xFF828282), // Change the color to #828282
                         shape: RoundedRectangleBorder(
@@ -169,16 +222,24 @@ class _BusinessSignUpScreenState extends State<BusinessSignUpScreen> {
                     ],
                   ),
                   SizedBox(height: 16),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: SocialLoginButtons(
-                      onGoogleLogin: () {
-                        print('Google Login Clicked');
-                      },
-                      onFacebookLogin: () {
-                        print('Facebook Login Clicked');
-                      },
-                    ),
+                  Column(
+                    children: [
+                      SocialMediaButton(
+                        onPressed: () {
+                          // Add your onPressed code here!
+                        },
+                        image: AssetImage('assets/google.png'),
+                        label: 'Continue with Google',
+                      ),
+                      SizedBox(height: 10),
+                      SocialMediaButton(
+                        onPressed: () {
+                          // Add your onPressed code here!
+                        },
+                        image: AssetImage('assets/facebook.png'),
+                        label: 'Continue with Facebook',
+                      ),
+                    ],
                   ),
                   SizedBox(height: 20),
                   Center(
@@ -209,10 +270,10 @@ class _BusinessSignUpScreenState extends State<BusinessSignUpScreen> {
                       ],
                     ),
                   ),
-                  const SizedBox(height: 100),
                 ],
               ),
             ),
+        ),
           ],
         ),
       ),
